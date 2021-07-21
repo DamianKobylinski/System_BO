9c528eeb11677dceefd3339d5bab78ae73d4b9b0
Jakub <jakubchorzepa@gmail.com>
2021-07-21 16:41:28 +0200

    Fixed routing bug

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Newsletter from '../views/Newsletter.vue';
 import MainForm from '../views/MainForm.vue';
 import AdminPanel from '../views/AdminPanel.vue';
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
